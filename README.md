9f697b3985d04440482d8ae38fc628afb83b2b0d
TaikiFnit <taikifnitspg@gmail.com>
2019-08-15 20:46:07 +0900

    line bot webhook on express server is now avaiable on cloud functions

---
+++ README.md
@@ -0,0 +1,5 @@
+# Minecraft BE Server: minecraftアカウントとLineを利用した2段階認証システム
+## About
+Minecraft Bedrock Edition Serverのwhitelist登録をLineBotを通じて行うようにする. 
+## 目的
+Lineアカウントを紐付けることによりMinecraft内での匿名性を排除し, 荒らし対策になるか検証する.
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,61 @@
 const functions = require('firebase-functions');
+const express = require('express');
 const axios = require('axios');
 
 // STEP1. Lineに友達登録
 // STEP2. Line bot nodejs serverがfirebaseにアクセスし, token発行&DBに保存.
 // STEP3. ユーザーがtoken付きのurlをふむ
 // Step4. public/index.html でtokenを読み取り, DBに保存.
+
+// LINE Webhook HANDLER
+const app = express();
+
+const line = require('@line/bot-sdk');
+const line_config = {
+    channelSecret: functions.config().line.channel_secret,
+    channelAccessToken: functions.config().line.channel_access_token
+};
+const client = new line.Client(line_config);
+
+app.post('/', line.middleware(line_config), async (req, res) => {
+    Promise
+      .all(req.body.events.map(handleLineEvent))
+      .then(result => res.json(result))
+      .catch(() => res.sendStatus(500));
+});
+
+async function handleLineEvent(event) {
+    if (event.type === 'follow') {
+  
+      // get user profile
+      const user_id = event.source.userId;
+      const profile = await axios.get(`https://api.line.me/v2/bot/profile/${user_id}`, {
+        headers: {
+          Authorization: `Bearer ${functions.config().line.channel_access_token}`,
+        }
+      });
+  
+      // send auth url
+      console.dir(profile.data);
+      const url = `?user_id=${profile.data.userId}`;
+  
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: `Hi! ${profile.data.displayName}. You can authorize via: ${profile.data.userId}`
+      });
+    }
+  
+    if (event.type === 'message' && event.message.type === 'text') {
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text //実際に返信の言葉を入れる箇所
+      });
+    }
+  
+    return Promise.resolve(null);
+}
+
+exports.line = functions.https.onRequest(app);
 
 // nounceの生成. sessionへ保存.
 exports.index = functions.https.onRequest(async (req, res) => {
